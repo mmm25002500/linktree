9dd594ab22bb31ee8801efdd69a776136d3cd17c
mmm25002500 <marryhan25648@gmail.com>
2023-04-29 00:18:43 +0800

    feat: add dark mode

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -13,8 +13,10 @@ const button = (props: Props) => {
     <div>
       <a href={ props.link } target="_blank">
         <button type="button" className={"w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none dark:focus:ring-blue-800 " + props.color}>
-          <FontAwesomeIcon icon={['fab', props.icon]} className="pr-2"/>
-          {props.text}
+          <FontAwesomeIcon icon={['fab', props.icon]} className="pr-2 text-white" />
+          <span className="text-white">
+            {props.text}
+          </span>
         </button>
       </a>
     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Navbar = () => {
-    return (
-        <div></div>
-    )
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') === 'true');
+
+  useEffect(() => {
+    if (darkMode) {
+    localStorage.setItem('darkMode', 'true');
+    document.documentElement.classList.add('dark');
+    } else {
+    localStorage.setItem('darkMode', 'false');
+    document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+  return (
+    <div>
+      <button onClick={() => setDarkMode(!darkMode)} type="button" className='absolute right-2 top-2'>
+        {
+          localStorage.getItem('darkMode') === 'true' ? (
+            <FontAwesomeIcon icon={['fas', 'sun']} className='pl-1 text-cyan-500' />
+          ) : (
+            <FontAwesomeIcon icon={['fas', 'moon']} className='pl-1 text-cyan-500' />
+          )
+        }
+      </button>
+    </div>
+  )
 }
 
 export default Navbar;
