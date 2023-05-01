7bfb1ddb9084c37c8ccc81b5a181c0b2b5636930
mmm25002500 <marryhan25648@gmail.com>
2023-05-01 21:18:18 +0800

    feat: create personal linktree

--- src/components/Settings/Profile.tsx
+++ src/components/Settings/Profile.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import { addDoc, collection, doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { addDoc, collection, doc, getDocs, getFirestore, query, setDoc, where } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 interface UserData {
   name: string;
   eng_name: string;
   nick: string;
   description: string;
-  email: string;
+  user_id: string;
+  uid: string;
   social_link: {
     fb: string;
     ig: string;
@@ -24,7 +25,8 @@ const Profile = () => {
     eng_name: '',
     nick: '',
     description: '',
-    email: '',
+    user_id: '',
+    uid: '',
     social_link: {
       fb: '',
       ig: '',
@@ -38,53 +40,123 @@ const Profile = () => {
 
   const [userData, setUserData] = useState<UserData>(user); 
   const [userID, setUserID] = useState<string>('');
+  const [Uid, setUid] = useState<string>('');
+  const [authUserID, setAuthUserID] = useState<string>('');
   const [userExist, setUserExist] = useState<boolean>(false);
+  const [all_user_id, setAllUserID] = useState<string[]>([]);
 
   const auth = getAuth();
   const db = getFirestore();
-  console.log(auth.currentUser?.uid);
+  const userCollRef = collection(db, "user");
+
+  useEffect(() => {
+    // get user id from auth
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid);
+
+        if (userData) {
+          const q = query(userCollRef, where("uid", "==", user.uid));
+          const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+            if (doc.data())
+              setUserExist(true);
+              setAuthUserID(doc.data().user_id);
+          });
+        }
+
+        // get all user_id
+        const usersRef = collection(db, 'user');
+        getDocs(usersRef)
+          .then(res => {
+            const usersData = res.docs.map(
+              doc => doc.data().user_id
+            );
+            setAllUserID(usersData);
+          })
+          .catch(err => {
+            console.log(err);
+          }
+        )
+      }
+    });
+  }, []);
+  
   
   // create user data in firestore
   const createNewData = () => {
     // const userCollRef = collection(db, "user");
     console.log(userData);
 
-    // get email from auth
-    const email = auth.currentUser?.email;
-
-    // add user data to firestore
-    setDoc(doc(db, 'user', userID), {...userData, email: email}).then(res => {
-      console.log(res);
-    }
-    ).catch(err => {
-      console.log(err);
-    })
-  }
-
-  // check user data in firestore
-  const checkData = (userID: string) => {
-    if (userID)
-    setUserExist(false);
-    
-    const userDocRef = doc(db, 'user', userID);
-    getDoc(userDocRef).then(res => {
-      if (res.exists()) {
-        setUserExist(true);
-      } else {
-        setUserExist(false);
+    // if user_id not in all_user_id
+    if (!all_user_id.includes(userData.user_id)) {
+      // add user data to firestore
+      setDoc(doc(db, 'user', userData.user_id), { ...userData, uid: Uid }).then(res => {
+        console.log(res);
       }
+      ).catch(err => {
+        console.log(err);
+      })
+    } else {
+      console.log('user_id already exist! do not use this user_id');
     }
-    ).catch(err => {
-      console.log(err);
-    }
-    )
   }
-
-  // edit user data in firestore
+ 
   const editData = () => {
+    console.log('userid:', userData.user_id)
+    console.log('authuserid:', authUserID)
+    if (userExist && userData.user_id === authUserID) {
 
+      // add user data to firestore
+      setDoc(doc(db, 'user', userData.user_id), { ...userData, uid: Uid }).then(res => {
+        console.log(res);
+      }
+      ).catch(err => {
+        console.log(err);
+      })
+    }
   }
 
+  const submit = () => {
+    // 檢查uid 是否在user collection中
+    if (userExist) {
+      // update user data
+      console.log('update');
+      editData();
+    } else {
+      // create new user data
+      console.log('create');
+      createNewData();
+    }
+  }
+
+  useEffect(() => {
+
+    const getData = async (uid: string) => {
+      const q = query(userCollRef, where("uid", "==", uid));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        if (doc.data())
+          setUserData(
+            {
+              name: doc.data().name,
+              eng_name: doc.data().eng_name,
+              nick: doc.data().nick,
+              description: doc.data().description,
+              user_id: doc.data().user_id,
+              uid: doc.data().uid,
+              social_link: doc.data().social_link
+            }
+          );
+      });
+    }
+    getData(Uid);
+  }, [userExist])
+
+  console.log(userData);
+  
+  // console.log(Uid);
+  // console.log(userExist);
   return (
     <div>
       <form>
@@ -92,34 +164,34 @@ const Profile = () => {
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           {/* Name */}
           <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">姓名</label>
-              <input onChange={e => setUserData({...userData, name: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">姓名</label>
+            <input value={ userData.name } onChange={e => setUserData({...userData, name: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
           </div>
 
           {/* eng_name */}
           <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">英文姓名</label>
-              <input onChange={e => setUserData({...userData, eng_name: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="English Name" required />
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">英文姓名</label>
+            <input value={ userData.eng_name } onChange={e => setUserData({...userData, eng_name: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="English Name" required />
           </div>
 
           {/* nick */}
           <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">暱稱</label>
-              <input onChange={e => setUserData({...userData, nick: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nick Name" required />
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">暱稱</label>
+            <input value={ userData.nick } onChange={e => setUserData({...userData, nick: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nick Name" required />
           </div>
 
           {/* user id */}
           {/* after creating, lock it */}
           <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">使用者ID</label>
-              <input onChange={e => setUserID(e.target.value)} type="test" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="User ID" required />
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">使用者ID</label>
+            <input value={ userData.user_id } onChange={e => setUserData({...userData, user_id: e.target.value})} type="test" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"disabled={userExist ? true : false} placeholder="User ID" required />
           </div>
         </div>
 
         {/* description */}
         <div className="mb-6">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">介紹一下你自己</label>
-          <input onChange={e => setUserData({...userData, description: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+          <input value={ userData.description } onChange={e => setUserData({...userData, description: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
         </div> 
 
         {/* website link */}
@@ -127,43 +199,43 @@ const Profile = () => {
           {/* FB */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Facebook</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, fb: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Facebook 網址" />
+            <input value={ userData.social_link.fb } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, fb: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Facebook 網址" />
           </div>
 
           {/* IG */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instagram</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, ig: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Instagram 網址" />
+            <input value={ userData.social_link.ig } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, ig: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Instagram 網址" />
           </div>
 
           {/* YT */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">YouTube</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, yt: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="YouTube 網址" />
+            <input value={ userData.social_link.yt } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, yt: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="YouTube 網址" />
           </div>
 
           {/* TG */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telegram</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, tg: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Telegram 網址" />
+            <input value={ userData.social_link.tg } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, tg: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Telegram 網址" />
           </div>
 
           {/* DC */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Discord</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, dc: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord 網址" />
+            <input value={ userData.social_link.dc }onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, dc: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord 網址" />
           </div>
 
           {/* Twitter */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Twitter</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, twitter: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Twitter 網址" />
+            <input value={ userData.social_link.twitter } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, twitter: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Twitter 網址" />
           </div>
 
           {/* Github */}
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Github</label>
-            <input onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, github: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Github 網址" />
+            <input value={ userData.social_link.github } onChange={e => setUserData({ ...userData, social_link: {...userData.social_link, github: e.target.value} })} type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Github 網址" />
           </div>
         </div>
 
@@ -176,7 +248,7 @@ const Profile = () => {
         </div>
 
         {/* after creating, changeg "建立" to "更改" */}
-        <button onClick={() => createNewData()} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">更改</button>
+        <button onClick={() => submit()} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">更改</button>
       </form>
 
       <input
@@ -192,7 +264,7 @@ const Profile = () => {
         value={userID}
         onChange={(e) => setUserID(e.target.value)}
       />
-      <button onClick={() => checkData(userID)}>search</button>
+      {/* <button onClick={() => checkData(userID)}>search</button> */}
       { userExist ? <div>exist</div> : <div>not exist</div>}
     </div>
   )
