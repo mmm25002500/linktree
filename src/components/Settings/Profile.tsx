f620294f9e878618da9d2e251d13a0666c18c699
mmm25002500 <marryhan25648@gmail.com>
2023-05-01 21:53:33 +0800

    fix: typo of image title

--- src/components/Settings/Profile.tsx
+++ src/components/Settings/Profile.tsx
@@ -199,7 +199,7 @@ const Profile = () => {
 
         {/* img_link */}
         <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">介紹一下你自己</label>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">更改您的大頭貼</label>
           <input value={ userData.img } onChange={e => setUserData({...userData, img: e.target.value})} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter image link" required />
         </div> 
 
