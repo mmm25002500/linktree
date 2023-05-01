30ac2e77d86c8fdaa4cd4ec41c8894d85392a121
mmm25002500 <marryhan25648@gmail.com>
2023-05-01 23:38:08 +0800

    feat: add linktree term

--- src/components/Settings/Profile.tsx
+++ src/components/Settings/Profile.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { addDoc, collection, doc, getDocs, getFirestore, query, setDoc, where } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface UserData {
   name: string;
@@ -46,6 +48,8 @@ const Profile = () => {
   const [authUserID, setAuthUserID] = useState<string>('');
   const [userExist, setUserExist] = useState<boolean>(false);
   const [all_user_id, setAllUserID] = useState<string[]>([]);
+  const [showPopover, setShowPopover] = useState<boolean>(false);
+  const [isTermCheck, setIsTermCheck] = useState<boolean>(false);
 
   const auth = getAuth();
   const db = getFirestore();
@@ -162,6 +166,40 @@ const Profile = () => {
   // console.log(userExist);
   return (
     <div>
+      {/* <!-- Term Modal --> */}
+      {showPopover && ReactDOM.createPortal(
+        <div className={`fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${showPopover ? '' : 'hidden'}`} onClick={() => setShowPopover(!showPopover)} >
+          <div className="relative m-auto w-full max-w-2xl max-h-full" onClick={(e) => e.stopPropagation()}>
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              {/* Modal header */}
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  靈萌團隊連結樹使用條款
+                </h3>
+                <button onClick={() => setShowPopover(!showPopover)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                  <FontAwesomeIcon icon={['fas', 'xmark']} className='w-5 h-5' />
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              {/* Modal body */}
+              <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  請遵守使用者條款
+                </p>
+              </div>
+
+              {/* Modal footer */}
+              <div onClick={() => {setIsTermCheck(true); setShowPopover(!showPopover)}} className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button> */}
+                <button onClick={() => setShowPopover(!showPopover)} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">看完了</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ,
+      document.body)}
+      
       <form>
         {/* personal information */}
         <div className="grid gap-6 mb-6 md:grid-cols-2">
@@ -251,9 +289,9 @@ const Profile = () => {
         {/* term */}
         <div className="flex items-start mb-6">
           <div className="flex items-center h-5">
-          <input type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+          <input type="checkbox"className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
           </div>
-          <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">我同意 <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">靈萌團隊連結樹使用條款</a>。</label>
+          <label onClick={() => setShowPopover(!showPopover)} className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">我同意 <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">靈萌團隊連結樹使用條款</a>。</label>
         </div>
 
         {/* after creating, changeg "建立" to "更改" */}
